e96fc1020423a4327fc4c7c5b4bd6aa87fa63d5c
Yevasee <e.saidyakov@yandex.ru>
2025-10-17 16:36:06 +0500

    2 version. features: search, sorter, filter

--- SaidyakovShoes/PageProducts.xaml.cs
+++ SaidyakovShoes/PageProducts.xaml.cs
@@ -25,11 +25,73 @@ namespace SaidyakovShoes
         {
             InitializeComponent();
             listProductsForPage = SaidyakovShoesEntities.GetContext().Product.ToList();
+            ComboBoxSorter.SelectedIndex = 0;
+            ComboBoxFilter.SelectedIndex = 0;
             UpdateProducts();
         }
         public void UpdateProducts()
         {
             listProductsForPage = SaidyakovShoesEntities.GetContext().Product.ToList();
+            ComboBoxSorterMaker();
+            ComboBoxFilterMaker();
+            TextBoxSearchMaker();
+        }
+
+        private void ComboBoxSorter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void ComboBoxFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void ComboBoxSorterMaker()
+        {
+            switch (ComboBoxSorter.SelectedIndex)
+            {
+                case 0: listProductsForPage = listProductsForPage.ToList(); break;
+                case 1: listProductsForPage = listProductsForPage.OrderBy(p => p.ProductQuantityInStock).ToList(); break;
+                case 2: listProductsForPage = listProductsForPage.OrderByDescending(p => p.ProductQuantityInStock).ToList(); break;
+            }
+
+            listViewProducts.ItemsSource = listProductsForPage;
+        }
+
+        private void ComboBoxFilterMaker()
+        {
+            switch (ComboBoxFilter.SelectedIndex)
+            {
+                case 0: listProductsForPage = listProductsForPage.ToList(); break;
+                case 1: listProductsForPage = listProductsForPage.Where(p => (p.ProductImporter.Equals("Kari"))).ToList(); break;
+                case 2: listProductsForPage = listProductsForPage.Where(p => (p.ProductImporter.Equals("Обувь для вас"))).ToList(); break;
+            }
+
+            listViewProducts.ItemsSource = listProductsForPage;
+        }
+        private void TextBoxSearchMaker()
+        {
+            string[] searchText = TextBoxSearch.Text.ToLower().Split(' ');
+            for (int i = 0; i < searchText.Length; i++)
+            {
+                listProductsForPage = listProductsForPage.Where(p => (p.ProductArticleNumber.ToLower().Contains(searchText[i])
+                                                                   || p.ProductName.ToLower().Contains(searchText[i])
+                                                                   || p.ProductUnit.ToLower().Contains(searchText[i])
+                                                                   || p.ProductCost.ToString().ToLower().Contains(searchText[i])
+                                                                   || p.ProductDiscountAmount.ToString().ToLower().Contains(searchText[i])
+                                                                   || p.ProductImporter.ToLower().Contains(searchText[i])
+                                                                   || p.ProductManufacturer.ToLower().Contains(searchText[i])
+                                                                   || p.ProductCategory.ToLower().Contains(searchText[i])
+                                                                   || p.ProductQuantityInStock.ToString().ToLower().Contains(searchText[i])
+                                                                   || p.ProductDescription.ToLower().Contains(searchText[i])
+                                                                   )).ToList();
+            }
 
             listViewProducts.ItemsSource = listProductsForPage;
         }
